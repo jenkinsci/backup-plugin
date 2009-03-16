e6a16611d839f9ff561256d6b5e1dea76250e9da
vsellier <vsellier@dev.java.net>
2009-03-16 00:09:14 +0000

    [backup-plugin]  put hudson in quiet mode before backup to avoid backuped files corruption

--- src/main/java/org/jvnet/hudson/plugins/backup/utils/BackupTask.java
+++ src/main/java/org/jvnet/hudson/plugins/backup/utils/BackupTask.java
@@ -1,7 +1,13 @@
 package org.jvnet.hudson.plugins.backup.utils;
 
+import hudson.model.Computer;
+import hudson.model.Hudson;
+
 import java.io.FileFilter;
 import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
@@ -11,6 +17,7 @@ import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.io.filefilter.NameFileFilter;
 import org.apache.commons.io.filefilter.NotFileFilter;
 import org.jvnet.hudson.plugins.backup.BackupException;
+import org.kohsuke.stapler.StaplerResponse;
 
 /**
  * This is the backup task, run in background and log to a file
@@ -19,10 +26,14 @@ import org.jvnet.hudson.plugins.backup.BackupException;
  * 
  */
 public class BackupTask extends BackupPluginTask {
+	/**
+	 * delay between two verification of no running processes
+	 */
+	private final static Integer DELAY = 5000;
 
-    private List<String> exclusions = new ArrayList<String>();
+	private List<String> exclusions = new ArrayList<String>();
 
-    public BackupTask() {
+	public BackupTask() {
 		exclusions.addAll(getDefaultsExclusions());
 	}
 
@@ -41,6 +52,9 @@ public class BackupTask extends BackupPluginTask {
 
 		logger.info("Backup started at " + getTimestamp(startDate));
 
+		// Have to include shutdown time in backup time ?
+		waitNoJobsInQueue();
+
 		FileFilter filter = createFileFilter(exclusions);
 
 		try {
@@ -49,21 +63,24 @@ public class BackupTask extends BackupPluginTask {
 			backupEngine.doBackup();
 		} catch (BackupException e) {
 			e.printStackTrace(logger.getWriter());
+		} finally {
+			cancelNoJobs();
+
+			endDate = new Date();
+			logger.info("Backup end at " + getTimestamp(endDate));
+			BigDecimal delay = new BigDecimal(endDate.getTime()
+					- startDate.getTime());
+			delay = delay.setScale(2, BigDecimal.ROUND_HALF_UP);
+			delay = delay.divide(new BigDecimal("1000"));
+
+			logger.info("[" + delay.toPlainString() + "s]");
+
+			finished = true;
+			logger.close();
 		}
-
-		endDate = new Date();
-		logger.info("Backup end at " + getTimestamp(endDate));
-		BigDecimal delay = new BigDecimal(endDate.getTime()
-				- startDate.getTime());
-		delay = delay.setScale(2, BigDecimal.ROUND_HALF_UP);
-		delay = delay.divide(new BigDecimal("1000"));
-
-		logger.info("[" + delay.toPlainString() + "s]");
-		finished = true;
-		logger.close();
 	}
 
-    public List<String> getDefaultsExclusions() {
+	public List<String> getDefaultsExclusions() {
 		List<String> defaultExclusion = new ArrayList<String>();
 
 		defaultExclusion.add("workspace");
@@ -71,7 +88,7 @@ public class BackupTask extends BackupPluginTask {
 		return defaultExclusion;
 	}
 
-    private FileFilter createFileFilter(List<String> exclusions) {
+	private FileFilter createFileFilter(List<String> exclusions) {
 		// creating the filter
 		IOFileFilter filter = new NameFileFilter(exclusions
 				.toArray(new String[] {}));
@@ -79,5 +96,58 @@ public class BackupTask extends BackupPluginTask {
 		filter = new NotFileFilter(filter);
 
 		return filter;
+	}
+
+	private void waitNoJobsInQueue() {
+		logger
+				.info("Setting hudson in shutdown mode to avoid files corruptions.");
+		try {
+			Hudson.getInstance().doQuietDown(getStaplerResponseFake());
+		} catch (Exception e) {
+			logger.error("Erreur putting hudson in shutdown mode.");
+			e.printStackTrace(logger.getWriter());
+		}
+
+		logger.info("Waiting all jobs end...");
+
+		Computer computer[] = Hudson.getInstance().getComputers();
+
+		int running = 1;
+		// TODO BACKUP-PLUGIN add timeout here
+		while (running > 0) {
+			running = 0;
+			for (int i = 0; i < computer.length; i++) {
+				running += computer[i].countBusy();
+			}
+			logger.info("Number of running jobs detected : " + running);
+			
+			try {
+				Thread.sleep(DELAY);
+			} catch (InterruptedException e) {
+			}
+		}
+
+		logger.info("All jobs finished.");
+	}
+
+	private void cancelNoJobs() {
+		logger.info("Cancel hudson shutdown mode");
+		try {
+			Hudson.getInstance().doCancelQuietDown(getStaplerResponseFake());
+		} catch (Exception e) {
+			logger.error("Erreur cancelling hudson shutdown mode.");
+			e.printStackTrace(logger.getWriter());
+		}
+	}
+
+	private StaplerResponse getStaplerResponseFake() {
+		return (StaplerResponse) Proxy.newProxyInstance(Thread.currentThread()
+				.getContextClassLoader(),
+				new Class[] { StaplerResponse.class }, new InvocationHandler() {
+					public Object invoke(Object proxy, Method method,
+							Object[] args) throws Throwable {
+						return null;
+					}
+				});
 	}
 }
