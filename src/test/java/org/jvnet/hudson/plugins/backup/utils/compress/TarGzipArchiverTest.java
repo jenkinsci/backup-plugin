eee7f3785c8d0688ea0f6d30ca2592276954aad9
vsellier <vsellier@dev.java.net>
2009-04-02 22:08:26 +0000

    [backup-plugin] Add test for tar gzip archiver

---
+++ src/test/java/org/jvnet/hudson/plugins/backup/utils/compress/TarGzipArchiverTest.java
@@ -0,0 +1,64 @@
+package org.jvnet.hudson.plugins.backup.utils.compress;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.codehaus.plexus.archiver.tar.TarGZipUnArchiver;
+import org.codehaus.plexus.archiver.zip.ZipUnArchiver;
+import org.codehaus.plexus.logging.console.ConsoleLogger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TarGzipArchiverTest {
+	private final static String OUTPUT_DIRECTORY = "target";
+	private final static String UNCOMPRESS_DIRECTORY = OUTPUT_DIRECTORY
+			+ "/uncompress";
+
+	/**
+	 * The archive result of the test, deleted at the end if every thing is ok
+	 */
+	File archiveFile;
+	/**
+	 * Place to unarchive the test, deleted at the end if every thing is ok
+	 */
+	File targetDirectory;
+
+	@Before
+	public void tearUp() throws Exception {
+		archiveFile = new File(OUTPUT_DIRECTORY, "test.tar.gz");
+
+		targetDirectory = new File(UNCOMPRESS_DIRECTORY);
+		targetDirectory.mkdir();
+	}
+
+	@Test
+	public void testArchiver() throws Exception {
+		TarGzipArchiver archiver = new TarGzipArchiver();
+		archiver.init(archiveFile);
+
+		ArchiverTestUtil.addTestFiles(archiver);
+		
+		TarGZipUnArchiver unarchiver = new TarGZipUnArchiver();
+		unarchiver.setSourceFile(archiveFile);
+
+		unarchiver.setDestDirectory(targetDirectory);
+		unarchiver.enableLogging(new ConsoleLogger(ConsoleLogger.LEVEL_DEBUG,
+				"Logger"));
+		unarchiver.extract();
+
+		Assert.assertTrue(ArchiverTestUtil.compareDirectoryContent(new File(Thread
+				.currentThread().getContextClassLoader().getResource("data")
+				.getFile()), targetDirectory));
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		archiveFile.delete();
+		FileUtils.deleteDirectory(targetDirectory);
+	}
+
+}
