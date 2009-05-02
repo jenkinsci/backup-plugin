b84bd01edddf502c60825925fbe1317a7d911615
vsellier <vsellier@dev.java.net>
2009-05-02 23:28:48 +0000

    [backup-plugin] deals with default values and add compression method

--- src/main/java/org/jvnet/hudson/plugins/backup/BackupConfig.java
+++ src/main/java/org/jvnet/hudson/plugins/backup/BackupConfig.java
@@ -1,5 +1,8 @@
 package org.jvnet.hudson.plugins.backup;
 
+import org.apache.commons.lang.StringUtils;
+import org.jvnet.hudson.plugins.backup.utils.compress.CompressionMethodEnum;
+
 /**
  * User: vsellier
  * Date: Apr 20, 2009
@@ -7,10 +10,12 @@ package org.jvnet.hudson.plugins.backup;
  */
 public class BackupConfig {
     private final static String DEFAULT_FILE_NAME_TEMPLATE = "backup_yyyyMMdd_HHmm";
+    private final static CompressionMethodEnum DEFAULT_COMPRESSION_METHOD = CompressionMethodEnum.ZIP;
 
     private String targetDirectory;
     private boolean verbose;
     private String fileNameTemplate = DEFAULT_FILE_NAME_TEMPLATE;
+    private CompressionMethodEnum archiveType = CompressionMethodEnum.ZIP;
 
     public boolean isVerbose() {
         return verbose;
@@ -30,10 +35,26 @@ public class BackupConfig {
     }
 
     public String getFileNameTemplate() {
-        return fileNameTemplate;
+        if (StringUtils.isNotEmpty(fileNameTemplate)) {
+            return fileNameTemplate;
+        } else {
+            return DEFAULT_FILE_NAME_TEMPLATE;
+        }
     }
 
     public void setFileNameTemplate(String fileNameTemplate) {
         this.fileNameTemplate = fileNameTemplate;
+    }
+
+    public CompressionMethodEnum getArchiveType() {
+        if (archiveType != null) {
+            return archiveType;
+        } else {
+            return DEFAULT_COMPRESSION_METHOD;
+        }
+    }
+
+    public void setArchiveType(CompressionMethodEnum archiveType) {
+        this.archiveType = archiveType;
     }
 }
