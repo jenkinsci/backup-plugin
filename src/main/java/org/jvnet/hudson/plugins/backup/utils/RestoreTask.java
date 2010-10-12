3b80487ec9f16f366d4d758a19aadabc05b4cc6b
dienomight <dienomight@dev.java.net>
2010-10-12 09:05:38 +0000

    bumping up POM version to 1.358 [FIXED HUDSON-7634] using FileUtils.copyDirectory to copy backup files to HUDSON_HOME instead of deleting the directory and renaming temporary one

--- src/main/java/org/jvnet/hudson/plugins/backup/utils/RestoreTask.java
+++ src/main/java/org/jvnet/hudson/plugins/backup/utils/RestoreTask.java
@@ -25,22 +25,17 @@
 package org.jvnet.hudson.plugins.backup.utils;
 
 import hudson.model.Hudson;
-import hudson.util.HudsonIsLoading;
+import org.apache.commons.io.FileUtils;
+import org.jvnet.hudson.plugins.backup.BackupConfig;
+import org.jvnet.hudson.plugins.backup.utils.compress.UnArchiver;
 
+import javax.servlet.ServletContext;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javax.servlet.ServletContext;
-
-import org.apache.commons.io.FileUtils;
-import org.jvnet.hudson.plugins.backup.BackupConfig;
-import org.jvnet.hudson.plugins.backup.utils.compress.UnArchiver;
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.StaplerResponse;
 
 /**
  * This is the restore task, run in background and log to a file
@@ -51,17 +46,9 @@ public class RestoreTask extends BackupPluginTask {
     private final static Logger LOGGER = Logger.getLogger(RestoreTask.class
             .getName());
 
-    /**
-     * This context will be passed to the StaplerRequest mock
-     * at the end of the restore. It is used be {@link Hudson} to put
-     * the {@link HudsonIsLoading} object.
-     */
-    private ServletContext servletContext;
-
     public RestoreTask(BackupConfig configuration, String hudsonWorkDir, String backupFileName,
                        String logFilePath, ServletContext servletContext) {
         super(configuration, hudsonWorkDir, backupFileName, logFilePath);
-        this.servletContext = servletContext;
     }
 
     public void run() {
@@ -107,28 +94,29 @@ public class RestoreTask extends BackupPluginTask {
             unAchiver.unArchive(archive, tempDirectoryPath);
         } catch (Exception e) {
             e.printStackTrace(logger.getWriter());
-            logger.error("Error uncompressiong archive : " + e.getMessage());
+            logger.error("Error uncompressing archive : " + e.getMessage());
             finished = true;
             return;
         }
 
-        // Not using tools like FileUtils.deleteDirectory
-        // because it is failing with non existing symbolic links
-        logger.info("Removing old configuration files...");
-        delete(directory);
-
-        logger.info("Making temporary directory the hudson home...");
-        temporary_directory.renameTo(directory);
+        //files from temporary directory will be copied to HUDSON_HOME, existing files will be overwritten
+        logger.info("Copying temporary directory to the hudson home...");
+        try {
+            FileUtils.copyDirectory(temporary_directory,directory);
+        } catch (IOException e) {
+            e.printStackTrace(logger.getWriter());
+            logger.error("Error copying backup files : " + e.getMessage());
+            finished = true;
+            return;
+        }
 
         logger.info("*****************************************");
         logger.info("Reloading hudson configuration from disk.");
         logger.info("*****************************************");
 
-        StaplerRequest request = FakeObject.getStaplerRequestFake(servletContext);
-        StaplerResponse response = FakeObject.getStaplerResponseFake();
 
         try {
-            Hudson.getInstance().doReload(request, response);
+            Hudson.getInstance().doReload();
         } catch (IOException e) {
             logger.error("Error reloading config files from disk.");
             logger.error("Call this method manually");
@@ -146,19 +134,5 @@ public class RestoreTask extends BackupPluginTask {
         finished = true;
         logger.close();
     }
-
-    private void delete(File file) {
-        if (file.isDirectory()) {
-            File[] files = file.listFiles();
-            for (int i = 0; i < files.length; i++) {
-                delete(files[i]);
-            }
-        }
-        if (configuration.isVerbose()) {
-            logger.debug("Deleting " + file.getAbsolutePath());
-        }
-        file.delete();
-    }
-
 
 }
